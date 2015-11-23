191df5974a00fda88cc64e101632e97e9a5e45f2
ricardoinacio <ricardoinacio@me.com>
2015-11-23 04:41:50 +0000

    Minimal part 2 completed

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -7,15 +7,14 @@ Given /the following movies exist/ do |movies_table|
     # you should arrange to add that movie to the database here.
     Movie.create!(movie)
   end
-  #visit '/movies'
-  #print page.html
-  #fail "Unimplemented"
 end
 
-    #visit '/movies/new'
-    #fill_in('movie_title', :with => movie['title'])
-    #fill_in('rating', :with => movie['rating'])
-    #fill_in('release_date', :with => movie['release_date'])
+### Comentários para consulta posterior
+#visit '/movies/new'
+#print page.html
+#fill_in('movie_title', :with => movie['title'])
+#fill_in('rating', :with => movie['rating'])
+#fill_in('release_date', :with => movie['release_date'])
 
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -31,12 +30,20 @@ end
 #  "When I uncheck the following ratings: PG, G, R"
 #  "When I check the following ratings: G"
 
-When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
+When /I (un)?check the following ratings: (.*)/ do |un, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  fail "Unimplemented"
+  rating_list.gsub(/\s/,'').split(',').each do |rating|
+    step %Q{I #{un}check "ratings_#{rating}"}
+  end
 end
+
+# Then /I should (not )?see the following movies: (.*)/ do |_not, movies_list|
+#   movies_list.gsub(/\s/,'').split(',').each do |movie|
+#     step %Q{I should see #{movie}}
+#   end
+# end
 
 Then /I should see all the movies/ do
   # Make sure that all the movies in the app are visible in the table
